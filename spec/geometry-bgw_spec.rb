73d0d31aef9f14391f76071862de2fe1ce40691f
Ben Winter <ben.g.winter@gmail.com>
2013-10-04 16:49:14 -0400

    added geo text

--- spec/geometry-bgw_spec.rb
+++ spec/geometry-bgw_spec.rb
@@ -1,7 +1,57 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "GeometryBgw" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+describe Geometry::Triangle do
+  
+  let(:a) { 3}
+  let(:b) { 4}
+  let(:c) { 5}
+
+  subject { Geometry::Triangle.new(a, b, c) }
+
+  its(:a) { should eq 3}
+  its(:b) { should eq 4}
+  its(:c) { should eq 5}
+
+  its(:perimeter) { should eq 12}
+  its(:area) { should eq 6}
+  its(:angles) { should eq [36.87, 53.13, 90.0]}
+
+  # checks if the subject is an instance of the correct class
+  it "should be an instance of geometry triangle" do
+    subject.should be_an_instance_of Geometry::Triangle
+
+end
+
+end
+
+
+describe Geometry::Rectangle do
+  
+  let(:length) { 4}
+  let(:width) { 6}
+
+
+  subject { Geometry::Rectangle.new(length, width) }
+
+  its(:length) { should eq 4}
+  its(:width) { should eq 6}
+
+  its(:area) { should eq 24}
+  its(:perimeter) { should eq 20}
+
+end
+
+
+describe Geometry::Circle do
+  
+  let(:radius) { 3}
+
+  subject { Geometry::Circle.new(radius) }
+
+  its(:radius) { should eq 3}
+
+  its(:circumference) { should eq 6 * Math::PI}
+
+  its(:area) { should eq 9 * Math::PI}
+
 end
